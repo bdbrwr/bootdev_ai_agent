ee4a20e962e58bc0a520ce0c2174cca48896a439
bdbrwr <bart@bdbrwr.nl>
2025-06-09 12:13:24 +0200

    Prompt from script arg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,8 +8,11 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    contents = "Why is Boot.dev such a great place to learn background development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=contents)
+    if len(sys.argv) != 2:
+        raise Exception("Please include a prompt -> usage python3 main.py PROMPT")
+
+    prompt = sys.argv[1]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
