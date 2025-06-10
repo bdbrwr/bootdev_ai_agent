eeabc115535bf278d7d6996d118161b9742c5efa
bdbrwr <bart@bdbrwr.nl>
2025-06-10 23:31:04 +0200

    Utilize list / types.Content for messages into prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -11,8 +12,11 @@ def main():
     if len(sys.argv) != 2:
         raise Exception("Please include a prompt -> usage python3 main.py PROMPT")
 
-    prompt = sys.argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
