a4a3dbe0b6acc673792c1990c9b73633142ebad6
bdbrwr <bart@bdbrwr.nl>
2025-06-10 23:43:58 +0200

    Align with bootdev solution: better way of handling flags as --verbose, integrating them in generate_content function too

--- main.py
+++ main.py
@@ -6,41 +6,41 @@ from google.genai import types
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
     
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = args[0]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = generate_content(client, messages)
-    verbose(args, response)
+    generate_content(client, messages, verbose)
 
-def generate_content(client, messages):
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
-    return response
-
-
-def verbose(args, response):
-    if "--verbose" not in args:
-        return
-    print("User prompt:", args[0])
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
