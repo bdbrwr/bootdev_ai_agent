0848e88823f32190da0e13d3608b3cb3fda3021e
bdbrwr <bart@bdbrwr.nl>
2025-06-10 23:33:47 +0200

    Align with bootdev solution: better processing / correction of parsing args. Generate content to its own function

--- main.py
+++ main.py
@@ -9,19 +9,33 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    if len(sys.argv) != 2:
-        raise Exception("Please include a prompt -> usage python3 main.py PROMPT")
+    args = sys.argv[1:]
 
-    user_prompt = sys.argv[1]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response: ")
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
     print(response.text)
+
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
