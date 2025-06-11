b194b8b45ccc8595611aad07f7e4b2f360a07af7
bdbrwr <bart@bdbrwr.nl>
2025-06-11 23:14:46 +0200

    Run local Python files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,7 +9,7 @@ def get_files_info(working_directory, directory=None):
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
-        return f'Erro: "{directory} is not a directory'
+        return f'Error: "{directory} is not a directory'
     try:
         files_info = []
         for filename in os.listdir(target_dir):
