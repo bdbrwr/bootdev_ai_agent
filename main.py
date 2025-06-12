00189522ea0055614ab82b0c82674a1a971e3167
bdbrwr <bart@bdbrwr.nl>
2025-06-12 23:45:08 +0200

    Integrate function calling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.genai import types
 from dotenv import load_dotenv
 
 from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -21,7 +22,7 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
+       
     user_prompt = " ".join(args)
 
     if verbose:
@@ -38,13 +39,18 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    
+    if not response.function_calls:
+        return response.text
+    
+    for functional_call_part in response.function_calls:
+        print(f"Calling function: {functional_call_part.name}({functional_call_part.args})")
 
 
 if __name__ == "__main__":
